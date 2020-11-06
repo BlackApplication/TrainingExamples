406d1e3966c15d6a41d294fa923061e7ad7d6838
Artyom <artemshimon2000@gmail.com>
2020-11-06 15:57:23 +0300

    Create and push some examples

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //Console.Write("Write your name: ");
+                //string name = Console.ReadLine();
+
+                //Console.Write("Write your age: ");
+                //int age = Convert.ToInt32(Console.ReadLine());
+
+                //Console.Write("Write your height: ");
+                //double height = Convert.ToDouble(Console.ReadLine());
+
+                //Console.Write("Write amount of salary: ");
+                //decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+                //Console.WriteLine($"Name: {name} Age: {age} Height: {height}m Salary: {salary}$");
+            }
+
+            {
+                //int a = 3;
+                //int b = 5;
+                //int c = 40;
+                //int d = c-- - b * a;    // a=3  b=5  c=39  d=25
+                //Console.WriteLine($"a={a}  b={b}  c={c}  d={d}");
+            }
+
+            {
+                int x = 45; //value  (101101)
+                int key = 102; //key  (1100110)
+                int encrypt = x ^ key; //encrypted value (1001011) - 75
+                Console.WriteLine("Encrypt value: " + encrypt);
+
+
+                int decrypt = encrypt ^ key; // decrypt key
+                Console.WriteLine("Decript value: " + decrypt);
+
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
